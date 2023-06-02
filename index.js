2c7b0271ef844a67a143fdce85f4ec44dfa2d329
Sarvarbek2003 <sarvarbekodilov070603@gmail.com>
2023-06-02 10:16:06 +0500

    for in added

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log("hello world")
 console.log("Hello world branch sarvar")
+let object = {text: 'Salomlar'}
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
